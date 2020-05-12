2b01a5a1f6338d134dfd9f426b7c72928b8017e1
ElDoctorino <42263991+ElDoctorino@users.noreply.github.com>
2020-05-12 14:04:53 +0300

    bonus3 korjattu

--- bonus3.js
+++ bonus3.js
@@ -1,44 +1,10 @@
-function contact(firstname, lastname, number, numberstring){
-    if(firstname === undefined || firstname === null || lastname === null || lastname === undefined || number === null || number === undefined){
+function contact(tiedot){
+    if(tiedot == null || tiedot == undefined){
         throw new Error('Etunimi, sukunimi tai numero on tyhjä.');
     }
-    if (contact != null) {
-        var numberstring = number.toString(); // Convertoidaan numero stringiksi
-        if (numberstring.length < 10) {
-            numberstring = "";
-            var full = {
-                firstname,
-                lastname,
-                numberstring,
-            };
-            full[0] ="<li>" + firstname + "</li>";
-            full[1] ="<li>" + lastname + "</li>";
-            full[2] ="<li>" + numberstring + "</li>";
-            return full;
-            //return full;
-        } else {
-            var full = {
-                firstname,
-                lastname,
-                numberstring,
-              };
-              firstname ="<li> " + firstname + " </li>";
-              lastname ="<li> " + lastname + " </li>";
-              numberstring ="<li> " + numberstring + " </li>";
-              var full = {
-                  firstname,
-                  lastname,
-                  numberstring
-              }
-              return full;
-        }
-    } else {
-        numberstring = "";
-        var full = {
-            firstname,
-            lastname,
-            numberstring,
-          };
+    if (tiedot != null) {
+            tiedot2 = "<li>" + tiedot.etunimi + ", " + tiedot.sukunimi + ", " + tiedot.numero + "</li>";
+            return tiedot2;
     }
 }
 module.exports =contact;
--- bonus3.test.js
+++ bonus3.test.js
@@ -5,15 +5,11 @@ test('Etunimi tai sukunimi puuttuu', () => {
         contact(firstname = null, lastname = null)
     }).toThrow('Etunimi, sukunimi tai numero on tyhjä.');
 });
+var tiedot = new Object();
+tiedot.etunimi = "Ville";
+tiedot.sukunimi = "Aaltonen";
+tiedot.numero = "0401260700";
 
 test('Testaa antaa perustiedot sekä puhelinnumero', () => {
-    expect(contact("Ville","Aaltonen","0401260700")).toEqual({
-        firstname: "<li> Ville </li>",
-        lastname: "<li> Aaltonen </li>",
-        numberstring: "<li> 0401260700 </li>"  
-    });
+    expect(contact(tiedot)).toEqual("<li>Ville, Aaltonen, 0401260700</li>");
 });
-//  test('Testaa tiedot', () => {
-//     (contact("Ville","Aaltonen","0401260700"))
-//     expect(contact(juusto).toContain('Ville', 'Aaltonen', '0401260700'));
-//    });
